74419e6d60c31f11ec34e3a416f207929d497239
Alan Reis <alanreisanjo@gmail.com>
2023-09-15 19:36:57 -0300

    main: Comentários adicionados

--- git-commit.sh
+++ git-commit.sh
@@ -5,6 +5,10 @@ description=$1
 if git rev-parse --verify HEAD >/dev/null 2>&1; then
     activeBranch=$(git rev-parse --abbrev-ref HEAD)
     git commit -m "$activeBranch: $description"
+    # git push
 else
     git commit "$description"
+    activeBranch=$(git rev-parse --abbrev-ref HEAD)
+    # git push -u origin $activeBranch
+    # TODO: Checar se há repositórios remotos
 fi
