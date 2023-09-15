b133974fa80f594379447d6ffc0c1c6018ebe72a
Alan Reis <alanreisanjo@gmail.com>
2023-09-15 19:31:29 -0300

    main: Adicionado o script

--- git-commit.sh
+++ git-commit.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-branch_atual=$(git rev-parse --abbrev-ref HEAD)
-echo $branch_atual
+description=$1
+
+if git rev-parse --verify HEAD >/dev/null 2>&1; then
+    activeBranch=$(git rev-parse --abbrev-ref HEAD)
+    git commit -m "$activeBranch: $description"
+else
+    git commit "$description"
+fi
