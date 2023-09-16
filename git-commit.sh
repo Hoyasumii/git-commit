15f332d29c8213a04c2f48777c01db090b75abb8
Alan Reis <alanreisanjo@gmail.com>
2023-09-16 03:05:08 -0300

    main: Ajuste no código para evitar redundância

--- git-commit.sh
+++ git-commit.sh
@@ -5,13 +5,11 @@ description=$1
 if git rev-parse --verify HEAD >/dev/null 2>&1; then
     activeBranch=$(git rev-parse --abbrev-ref HEAD)
     git commit -m "$activeBranch: $description"
-    if git remote -v | grep -q "fetch"; then
-        git push;
-    fi
 else
     git commit "$description"
     activeBranch=$(git rev-parse --abbrev-ref HEAD)
-    if git remote -v | grep -q "fetch"; then
-        git push -u origin $activeBranch
-    fi
+fi
+
+if git remote -v | grep -q "fetch"; then
+    git push -u origin $activeBranch
 fi
